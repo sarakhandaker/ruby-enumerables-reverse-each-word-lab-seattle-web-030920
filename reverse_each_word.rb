3783bc5132c7614d15198bc78a29d22a0faaaf39
Sara Khandaker <kttsara@hotmail.com>
2020-02-29 04:21:22 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,17 +1,17 @@
+#def reverse_each_word(sentence)
+#  array=sentence.split(" ")
+#  new_arr=[]
+#array.each do |word|
+ #   new_arr<< word.reverse
+ # end
+ # string=new_arr.join(" ")
+#end
+
 def reverse_each_word(sentence)
   array=sentence.split(" ")
-  new_arr=[]
-array.each do |word|
+ new_arr=[]
+  array.collect do |word|
     new_arr<< word.reverse
-  end
-  string=new_arr.join(" ")
-end
-
-#def reverse_each_word(sentence)
- # array=sentence.split(" ")
-# new_arr=[]
- # array.collect do |word|
- #   new_arr<< word.reverse
-# end
-# string= new_arr.join(' ')
-# end
+ end
+ string= new_arr.join(' ')
+ end
