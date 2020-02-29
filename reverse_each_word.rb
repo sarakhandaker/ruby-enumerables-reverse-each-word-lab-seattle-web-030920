1996730ba2e8566dd5abd31c1516093d569432f2
Sara Khandaker <kttsara@hotmail.com>
2020-02-29 04:14:47 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,10 @@
 def reverse_each_word(sentence)
   array=sentence.split(" ")
   
- newarr= array.each do |word|
+array.each do |word|
     word.reverse
   end
-  string= newarr.join(' ')
+
 end
 
 #def reverse_each_word(sentence)
