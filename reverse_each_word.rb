da7a66afb5a4b61fc4636dec5cb51505ef0d12c4
Sara Khandaker <kttsara@hotmail.com>
2020-02-29 04:25:10 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,17 @@
+#def reverse_each_word(sentence)
+#  array=sentence.split(" ")
+#  new_arr=[]
+#array.each do |word|
+ #   new_arr<< word.reverse
+ # end
+ # string=new_arr.join(" ")
+#end
+
+def reverse_each_word(sentence)
+  array=sentence.split(" ")
+ new_arr=[]
+  array.collect do |word|
+    new_arr<< word.reverse
+ end
+ new_arr.join(" ")
+ end
