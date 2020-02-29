2c1c70033904089b48ddcdd6c1f04d80d81b33af
Sara Khandaker <kttsara@hotmail.com>
2020-02-29 04:05:26 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,12 +2,14 @@ def reverve_word(sentence)
   array=sentence.split(" ")
   
   array.each do ||
-  
+    
+  end
 end
 
 def reverve_word(sentence)
   array=sentence.split(" ")
   
   array.collect do ||
-  
+    
+  end
 end
